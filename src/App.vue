30c36377d6fb9630d84c4ba404ebef0c1fe844e3
Vuk Kapor <vukkapor12@gmail.com>
2019-07-15 20:38:58 +0200

    empty

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/movies">Movies</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/AppMovies.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>nesto</div>
+</template>
+
+<script>
+export default {};
+</script>
