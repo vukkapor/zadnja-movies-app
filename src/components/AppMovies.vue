5f46839f18a1c3a93e74ab90df675a125ee18a9f
Vuk Kapor <vukkapor12@gmail.com>
2019-07-15 21:06:02 +0200

    movies show all

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -1,7 +1,48 @@
 <template>
-  <div>nesto</div>
+  <div>
+    <table style="width:100%">
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Director</th>
+        <th>Image url</th>
+        <th>Duration</th>
+        <th>Release date</th>
+        <th>Genre</th>
+      </tr>
+      <tr v-for="movie in movies" :key="movie.id">
+        <td>{{movie.id}}</td>
+        <td>{{movie.title}}</td>
+        <td>{{movie.director}}</td>
+        <td>{{movie.imageUrl}}</td>
+        <td>{{movie.duration}}</td>
+        <td>{{movie.releaseDate}}</td>
+        <td>{{movie.genre}}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {};
+import { moviesService } from "../services/movies";
+export default {
+  data() {
+    return {
+      movies: []
+    };
+  },
+
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      moviesService
+        .getAll()
+        .then(response => {
+          vm.movies = response.data;
+        })
+        .catch(e => {
+          alert(e);
+        });
+    });
+  }
+};
 </script>
--- src/services/movies.js
+++ src/services/movies.js
@@ -4,4 +4,10 @@ export class MoviesService {
   constructor() {
     axios.defaults.baseURL = 'http://localhost:3000/api/'
   }
+
+  getAll() {
+    return axios.get('movies');
+  }
 }
+
+export const moviesService = new MoviesService();
